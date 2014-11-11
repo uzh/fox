f8fc394927d42427e308fc0d61e292ba3b67123e
Philip Stutz <philip.stutz@gmail.com>
2014-11-11 13:20:17 +0100

    Update Build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -3,6 +3,6 @@ import Keys._
 
 object GraphsBuild extends Build {
   lazy val scCore = ProjectRef(file("../signal-collect"), id = "signal-collect")
-  val wolf = Project(id = "wolf",
+  val fox = Project(id = "fox",
     base = file(".")) dependsOn (scCore)
 }
