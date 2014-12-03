f9e552171791a6c62b0be05f88b71283370f86d1
Sara Magliacane <sara.magliacane@gmail.com>
2014-12-03 13:53:23 +0100

    New MovieRecommendation example

--- src/test/scala/com/signalcollect/psl/examples/MovieRecommendationsWithClasses.scala
+++ src/test/scala/com/signalcollect/psl/examples/MovieRecommendationsWithClasses.scala
@@ -29,49 +29,56 @@ import com.signalcollect.util.TestAnnouncements
 
 class MovieRecommendationsWithClasses extends FlatSpec with Matchers with TestAnnouncements {
 
-  val movieExampleWithClasses = """
-    class Movie: pulp-fiction, trainspotting, mission-impossible, blade-runner, star-wars, grease
-    class Person: sara, philip, fred
-	class Actor: john-travolta, tom-cruise, ewan-mcgregor, samuel-l-jackson
-    
-	predicate: likes(Person, _)
-	predicate: playsIn(Actor, Movie)
-	    
-	rule [weight = 1]: likes(PERSON, MOVIE) && playsIn(ACTOR, MOVIE) => likes(PERSON, ACTOR)
-	rule [weight = 1]: likes(PERSON, MOVIE-A) && playsIn(ACTOR, MOVIE-B) && playsIn(ACTOR, MOVIE-B) => likes(PERSON, MOVIE-B)
-	rule [weight = 1]: likes(PERSON-A, A) && likes(PERSON-B, A) && likes(PERSON-B, B) => likes(PERSON-A, B)
+  val movieExample2 = """
+class User: sara, philip, fred, george
+class Movie: mission-impossible, rear-window, vertigo, top-gun, blade-runner, indiana-jones-and-the-last-crusade, star-wars, zelig, pulp-fiction
+class Actor: john-travolta, samuel-l-jackson, ewan-mcgregor, tom-cruise, woody-allen
+class Director: woody-allen, quentin-tarantino, ridley-scott
+class Likable: woody-allen, quentin-tarantino, ridley-scott, john-travolta, samuel-l-jackson, ewan-mcgregor, tom-cruise, mission-impossible, rear-window, vertigo, top-gun, blade-runner, indiana-jones-and-the-last-crusade, star-wars, zelig, pulp-fiction
 
-	fact: playsIn(john-travolta, pulp-fiction)
-	fact: playsIn(samuel-l-jackson, pulp-fiction)
-	fact: playsIn(ewan-mcgregor, trainspotting)
-	fact: playsIn(tom-cruise, mission-impossible)
-	
-	fact [truthValue = 0.1]: likes(sara, tom-cruise)
-	fact [truthValue = 0.5]: likes(sara, john-travolta)
-	fact [truthValue = 0.9]: likes(sara, samuel-l-jackson)
-	fact [truthValue = 1.0]: likes(sara, ewan-mcgregor)
-	
-	fact [truthValue = 0.9]: likes(sara, pulp-fiction)
-	fact [truthValue = 0.3]: likes(sara, grease)
-	fact [truthValue = 0.8]: likes(sara, star-wars)
-	fact [truthValue = 0.8]: likes(sara, trainspotting)
-	fact [truthValue = 0.8]: likes(sara, blade-runner)
-	
-	fact [truthValue = 0.9]: likes(philip, pulp-fiction)
-	fact [truthValue = 0.8]: likes(philip, john-travolta)
-	fact [truthValue = 1.0]: likes(philip, blade-runner)
-	
-	fact [truthValue = 1.0]: likes(fred, trainspotting) 
+predicate [prior = 0.5]: likes(User, Likable)
+predicate [prior = 0]: playsIn(Actor, Movie)
+predicate [Functional]: directedBy(Movie, Director)
+predicate [Symmetric]: friends(User, User)
+
+// Each director directed at least a movie.
+rule [weight = 1000]: EXISTS[MOVIE] directedBy(MOVIE, DIRECTOR)
+rule [weight = 5]:   playsIn(ACTOR-DIRECTOR,MOVIE) => directedBy(MOVIE, ACTOR-DIRECTOR)
+
+// Like an actor, like movie with the actor.
+rule [weight = 5]:   likes(USER-A, ACTOR) && playsIn(ACTOR,MOVIE) => likes(USER-A,MOVIE)
+rule [weight = 1]:   likes(USER-A, MOVIE) && playsIn(ACTOR,MOVIE) => likes(USER-A,ACTOR)
+
+// Like a director, like the movie with the director.
+rule [weight = 2]:   likes(USER-A, DIRECTOR) && directedBy(MOVIE, DIRECTOR) => likes(USER-A,MOVIE)
+rule [weight = 10]:   likes(USER-A,MOVIE) && directedBy(MOVIE,DIRECTOR) => likes(USER-A,DIRECTOR)
+// Like what your friends like.
+// likesAL + friendsAB - 1 - likesBL
+// likesAL = 0.9 => friendsAB - likesBL - 0.1 => friendsAB <= likesBL + 0.1
+rule [weight = 1]:   likes(USER-A, LIKABLE) && friends(USER-A,USER-B) => likes(USER-B,LIKABLE)
+// Like what similar users like, if they like at least two similar things.
+rule [weight = 0.1]: likes(USER-A, LIKABLE) && likes(USER-A, LIKABLE2) && likes(USER-B,LIKABLE)  => likes(USER-B,LIKABLE2)
+ 
+fact: playsIn(tom-cruise, top-gun)
+fact: playsIn(tom-cruise, mission-impossible)
+fact: playsIn(woody-allen, zelig)
+fact: playsIn(samuel-l-jackson, pulp-fiction)
+fact: directedBy(pulp-fiction, quentin-tarantino)
+fact: !playsIn(samuel-l-jackson, zelig)
+
+fact [truthValue = 0.9]: friends(sara, philip)
+fact [truthValue = 0.1]: likes(sara, tom-cruise)
+fact [truthValue = 0.8]: likes(sara, pulp-fiction)
+
+fact [truthValue = 0.9]: likes(philip, pulp-fiction)
+fact [truthValue = 0.9]: likes(fred, tom-cruise)
 	"""
 
   "MovieRecommendationsWithClasses" should "correctly minimize the simplified movie example" in {
     val config = InferencerConfig(computeObjectiveValueOfSolution = true)
-    val inferenceResults = Inferencer.runInferenceFromString(movieExampleWithClasses, config = config)
-
-    val solution = inferenceResults.solution
-    val gps = inferenceResults.idToGpMap
+    val inferenceResults = Inferencer.runInferenceFromString(movieExample2, config = config)
     val objectiveFunctionValOption = inferenceResults.objectiveFun
     assert(objectiveFunctionValOption.isDefined)
   }
-    
+
 }
